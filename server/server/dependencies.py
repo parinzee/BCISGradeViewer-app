a65e35e5649f8063f2b814097770698c44e276d7
Parinz <parinzee@protonmail.com>
2021-10-08 17:29:49 +0700

    Server: More Oauth Implementation

--- server/server/dependencies.py
+++ server/server/dependencies.py
@@ -1,6 +1,21 @@
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends
+from pydantic import BaseModel
 
 # Depends on the oauth scheme
 oauthScheme = OAuth2PasswordBearer(tokenUrl="token")
-oauthSchemeDependecy = Depends(oauthScheme)
+
+# Create a user class for oauth
+class OauthUser(BaseModel):
+    username: str
+
+
+# Token decoder
+def oauth_token_decoder(token: str):
+    return OauthUser(
+        username=token + "decoded",
+    )
+
+
+async def get_current_user(token: str = Depends(oauthScheme)):
+    return oauth_token_decoder(token)
--- server/server/routers/root.py
+++ server/server/routers/root.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from ..dependencies import oauthSchemeDependecy
+from ..dependencies import get_current_user, oauthScheme, OauthUser
 from ..scraper.user import User
 
-router = APIRouter()
+router = APIRouter(tags=["User"])
 
 
-@router.get("/get_all")
-async def get_all(token: str = oauthSchemeDependecy):
+@router.get("/me/")
+async def get_current_user(current_user: OauthUser):
+    return current_user
+
+
+@router.get("/get_all/")
+async def get_all(token: str = Depends(oauthScheme)):
     return {"token": token}
