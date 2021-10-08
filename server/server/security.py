dc1139fb4c125e47dc35c97944ac3221fb5e6d61
Parinz <parinzee@protonmail.com>
2021-10-08 23:17:44 +0700

    Server/security: Added status codes for JWT decoding

--- server/server/security.py
+++ server/server/security.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
 from pydantic import BaseModel
 
 from .config import ACCESS_TOKEN_EXPIRE_DAYS, ALGORITHM, SECRET_KEY
@@ -18,7 +18,15 @@ class OauthUser(BaseModel):
 
 # Oauth, gets current user by jwt decoding.
 async def get_current_user(token: str = Depends(oauthScheme)):
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        raise credentials_exception
     username: str = payload.get("name")
     password: str = payload.get("pass")
     return {"username": username, "password": password}
