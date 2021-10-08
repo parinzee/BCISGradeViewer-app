091c4047c37be2c696b76cd7067ad90fa53d5f55
Parinz <parinzee@protonmail.com>
2021-10-08 19:50:05 +0700

    Server: Removed already done #TODO

--- server/server/routers/root.py
+++ server/server/routers/root.py
@@ -6,7 +6,6 @@ from ..scraper.user import User
 router = APIRouter(tags=["User"])
 
 
-# TODO: Return all values from scraper
 @router.get("/get_all/")
 async def get_all(userDetails: OauthUser = Depends(get_current_user)):
     user = User(userDetails["username"], userDetails["password"])
